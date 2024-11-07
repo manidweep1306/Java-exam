e50d3d2728e6cdd67074b5d473fa188d37e93533
manidweep1306 <166938022+manidweep1306@users.noreply.github.com>
2024-11-07 08:09:14 +0530

    Create c)-b-search.java

---
+++ c)-b-search.java
@@ -0,0 +1,41 @@
+import java.util.*;
+
+public class Search {
+	public static int binarySearch(int[] a, int low, int high, int key) {
+		while (low <= high) {
+			int mid = (low + high) / 2;
+			if (a[mid] == key)
+				return mid;
+			else if (a[mid] < key)
+				low = mid + 1;
+			else
+				high = mid - 1;
+		}
+		return -1;
+	}
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter the number of elements in the array: ");
+		int n = sc.nextInt();
+		int[] arr = new int[n];
+
+		System.out.println("Enter the elements: ");
+		for (int i = 0; i < n; i++) {
+			arr[i] = sc.nextInt();
+		}
+		Arrays.sort(arr);
+
+		System.out.println("Enter the key: ");
+		int key = sc.nextInt();
+
+		int pos = binarySearch(arr, 0, n - 1, key);
+
+		if (pos == -1)
+			System.out.println("Key is not found.");
+		else
+			System.out.println("Key is found at index " + pos);
+
+		sc.close();
+	}
+}
